7fd834256bb08cc1a1e0744d137b27685b20ad86
Hope-you <595318443@qq.com>
2021-11-21 18:51:08 +0800

    模块化(axios方法相关)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>{{ nowTime }}</div>
-  <el-button type="success" size="medium" @click="getNowTime">显示</el-button>
+  <h2>请选择食物</h2>
+  <div>随机选择食物</div>
+  <div v-if="loading">正在加载...</div>
+  <div v-else>
+    <!-- 不知道为啥,ts提示可能为空,但是可以运行预览 -->
+    <el-table :data="result.data" stripe style="width:50%">
+      <el-table-column prop="alg" label="状态" />
+      <el-table-column prop="searchWord" label="标题" width="180" />
+      <el-table-column prop="content" label="内容" width="180" />
+      <el-table-column prop="score" label="内容id" />
+    </el-table>
+  </div>
 </template>
 
 <script lang="ts">
-
-//引入hooks中自定义的模块
-import { nowTime, getNowTime } from './hooks/useNowTime';
+import useUrlAxios from "./hooks/useUrlAxios";
+//axios
 export default {
   name: 'App',
   setup() {
+    const { result, loading, loaded } = useUrlAxios("http://true.al.52hz.cyou:3000/search/hot/detail");
+    console.log(result)
     return {
-      nowTime, getNowTime
+      result, loading, loaded
     }
   },
 };
